37c71750636a22007b7e243ea184f3f73ebadc6e
Midoria7 <1049943690@qq.com>
2024-06-26 18:30:13 +0800

    Feature: ClientHandler & Update UserManager

--- src/main/java/javaChatRoom/server/clientHandler/ClientHandler.java
+++ src/main/java/javaChatRoom/server/clientHandler/ClientHandler.java
@@ -1,4 +1,98 @@
 package javaChatRoom.server.clientHandler;
 
-public class ClientHandler {
+import javaChatRoom.common.message.Message;
+import javaChatRoom.common.command.Command;
+import javaChatRoom.server.userManager.UserManager;
+import javaChatRoom.common.communicationManager.CommunicationManager;
+import javaChatRoom.server.serverLogger.ServerLogger;
+
+import java.net.Socket;
+import java.util.List;
+
+public class ClientHandler extends Thread {
+    private Socket socket;
+    private UserManager userManager;
+    private CommunicationManager commManager;
+
+    public ClientHandler(Socket socket, UserManager userManager) {
+        this.socket = socket;
+        this.userManager = userManager;
+        try {
+            this.commManager = new CommunicationManager(socket);
+        } catch (Exception e) {
+            ServerLogger.writeError("Error setting up Communication Manager: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!socket.isClosed()) {
+                Object obj = commManager.receiveObject();
+                if (obj instanceof Message) {
+                    handleMessage((Message) obj);
+                } else if (obj instanceof Command) {
+                    handleCommand((Command) obj);
+                }
+            }
+        } catch (Exception e) {
+            ServerLogger.writeError("Error handling client connection: " + e.getMessage());
+        } finally {
+            commManager.close();
+        }
+    }
+
+    private void handleMessage(Message message) {
+        if (message.getIsBroadcast()) {
+            broadcastMessage(message);
+        } else {
+            sendMessage(message, message.getReceiver());
+        }
+    }
+
+    private void broadcastMessage(Message message) {
+        List<ClientHandler> clients = userManager.getConnectedClients();
+        for (ClientHandler client : clients) {
+            if (client != this) {
+                String sender = message.getIsAnonymous() ? "ANONYMOUS" : message.getSender();
+                client.commManager.sendObject(new Message(sender, message.getReceiver(), message.getContent(), true, true));
+            }
+        }
+    }
+
+    private void sendMessage(Message message, String sender) {
+        Message outgoingMessage = new Message(sender, message.getReceiver(), message.getContent(), message.getIsBroadcast(), message.getIsAnonymous());
+        commManager.sendObject(outgoingMessage);
+    }
+
+    private void handleCommand(Command command) {
+        if (command.getCommandType() == Command.CommandType.USERLIST) {
+            sendUserList();
+        } else if (command.getCommandType() == Command.CommandType.LOGINREQUEST) {
+            String username = command.getArgs()[0];
+            String password = command.getArgs()[1];
+            if (userManager.isValidUser(username, password)) {
+                userManager.userLogIn(username, this);
+                commManager.sendObject(new Command(Command.CommandType.LOGINRESPONSE, "SUCCESS"));
+            } else {
+                commManager.sendObject(new Command(Command.CommandType.LOGINRESPONSE, "FAILURE"));
+            }
+        } else if (command.getCommandType() == Command.CommandType.REGISTERREQUEST) {
+            String username = command.getArgs()[0];
+            String password = command.getArgs()[1];
+            if (userManager.registerUser(username, password)) {
+                commManager.sendObject(new Command(Command.CommandType.REGISTERRESPONSE, "SUCCESS"));
+            } else {
+                commManager.sendObject(new Command(Command.CommandType.REGISTERRESPONSE, "FAILURE"));
+            }
+        } else if (command.getCommandType() == Command.CommandType.QUIT) {
+            userManager.userLoggedOut(this);
+            commManager.close();
+        }
+    }
+
+    private void sendUserList() {
+        List<String> onlineUsers = userManager.getOnlineUsernames();
+        commManager.sendObject(onlineUsers);
+    }
 }
--- src/main/java/javaChatRoom/server/userManager/UserManager.java
+++ src/main/java/javaChatRoom/server/userManager/UserManager.java
@@ -1,51 +1,64 @@
 package javaChatRoom.server.userManager;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
+import java.util.stream.Collectors;
+import javaChatRoom.server.clientHandler.ClientHandler;
 import javaChatRoom.server.serverLogger.ServerLogger;
 
 public class UserManager {
     private List<User> users;
-    private List<User> onlineUsers;
+    private List<String> onlineUsers;
+    private Map<String, ClientHandler> userConnections;
     private static final String USER_DATA_PATH = "user.ser";
 
     public UserManager() {
-        users = new ArrayList<>();
-        onlineUsers = new ArrayList<>();
+        users = Collections.synchronizedList(new ArrayList<>());
+        onlineUsers = Collections.synchronizedList(new ArrayList<>());
+        userConnections = new HashMap<>();
         loadUsers();
     }
 
-    public void clearUsers() {
+    public synchronized void clearUsers() {
         users.clear();
         saveUsers();
     }
 
-    private void loadUsers() {
+    private synchronized void loadUsers() {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(USER_DATA_PATH))) {
             users = (List<User>) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             ServerLogger.writeError("Error loading users: " + e.getMessage());
+            users = Collections.synchronizedList(new ArrayList<>()); // Start with an empty list if loading failed
         }
     }
 
-    public boolean isValidUser(String username, String password) {
+    public synchronized boolean isValidUser(String username, String password) {
         return users.stream()
                 .anyMatch(user -> user.getUsername().equals(username) && user.getPassword().equals(password));
     }
 
-    public void registerUser(String username, String password) {
-        users.add(new User(username, password));
-        saveUsers();
+    public synchronized boolean registerUser(String username, String password) {
+        if (users.stream().noneMatch(u -> u.getUsername().equals(username))) {
+            User newUser = new User(username, password);
+            users.add(newUser);
+            saveUsers();
+            return true; // Registration successful
+        }
+        return false; // Username already exists
     }
 
-    public void addUser(User user) {
+    public synchronized void addUser(User user) {
         users.add(user);
         saveUsers();
     }
 
-    private void saveUsers() {
+    public synchronized void removeUser(User user) {
+        users.remove(user);
+        saveUsers();
+    }
+
+    private synchronized void saveUsers() {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(USER_DATA_PATH))) {
             oos.writeObject(users);
         } catch (IOException e) {
@@ -53,17 +66,33 @@ public class UserManager {
         }
     }
 
-    public void userLoggedIn(User user) {
+    public synchronized void userLogIn(String user, ClientHandler handler) {
         if (!onlineUsers.contains(user)) {
             onlineUsers.add(user);
+            userConnections.put(user, handler);
+        } else {
+            ServerLogger.writeWarning("User " + user + " is already logged in");
         }
     }
 
-    public void userLoggedOut(User user) {
-        onlineUsers.remove(user);
+    public synchronized void userLoggedOut(ClientHandler handler) {
+        String username = onlineUsers.stream()
+                .filter(user -> userConnections.get(user).equals(handler))
+                .findFirst()
+                .orElse(null);
+        if (username != null) {
+            onlineUsers.remove(username);
+            userConnections.remove(username);
+        } else {
+            ServerLogger.writeError("ClientHandler not found in userConnections");
+        }
     }
 
-    public List<User> getOnlineUsers() {
+    public synchronized List<ClientHandler> getConnectedClients() {
+        return new ArrayList<>(userConnections.values());
+    }
+
+    public synchronized List<String> getOnlineUsernames() {
         return new ArrayList<>(onlineUsers);
     }
 }
