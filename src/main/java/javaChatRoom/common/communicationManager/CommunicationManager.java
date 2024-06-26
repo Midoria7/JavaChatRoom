73d549a492ba73f9b7d29f333d6e4e0389429959
Midoria7 <1049943690@qq.com>
2024-06-26 17:37:08 +0800

    Feature: CommunicationManager

---
+++ src/main/java/javaChatRoom/common/communicationManager/CommunicationManager.java
@@ -0,0 +1,52 @@
+package javaChatRoom.common.communicationManager;
+
+import javaChatRoom.server.serverLogger.ServerLogger;
+
+import java.io.*;
+import java.net.Socket;
+
+public class CommunicationManager {
+    private final Socket socket;
+    private final ObjectOutputStream outputStream;
+    private final ObjectInputStream inputStream;
+
+    public CommunicationManager(Socket socket) throws IOException {
+        this.socket = socket;
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+        inputStream = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void sendObject(Object obj) {
+        try {
+            outputStream.writeObject(obj);
+            outputStream.flush();
+        } catch (IOException e) {
+            ServerLogger.writeError("Error sending object: " + e.getMessage());
+        }
+    }
+
+    public Object receiveObject() {
+        try {
+            return inputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            ServerLogger.writeError("Error receiving object: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void close() {
+        try {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            ServerLogger.writeError("Error closing connections: " + e.getMessage());
+        }
+    }
+}
