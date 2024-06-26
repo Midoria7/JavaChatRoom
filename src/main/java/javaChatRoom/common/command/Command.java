f58baf1e98126b9b241db472742d5a9457869ea8
Midoria7 <1049943690@qq.com>
2024-06-26 17:52:34 +0800

    Feature: Update Command

--- src/main/java/javaChatRoom/common/command/Command.java
+++ src/main/java/javaChatRoom/common/command/Command.java
@@ -4,16 +4,21 @@ import java.io.Serializable;
 
 public class Command implements Serializable {
     public enum CommandType {
-        USERLIST, QUIT
+        USERLIST, QUIT, REGISTERREQUEST, REGISTERRESPONSE, LOGINREQUEST, LOGINRESPONSE
     }
+    private final String[] args;
+    private final CommandType commandType;
 
-    private CommandType commandType;
-
-    public Command(CommandType commandType) {
+    public Command(CommandType commandType, String... args) {
         this.commandType = commandType;
+        this.args = args;
     }
 
     public CommandType getCommandType() {
         return commandType;
+    }
+
+    public String[] getArgs() {
+        return args;
     }
 }
